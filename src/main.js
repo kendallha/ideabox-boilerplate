f4d8afc13b6fb26bfe0a358ebba1030d30b94503
kendallha <kendallhaw@gmail.com>
2021-02-24 13:12:29 -0800

    Add new Idea class and write query selector variables for input form

---
+++ src/main.js
@@ -0,0 +1,11 @@
+//query selector variables
+var saveButton = document.querySelector("#saveButton");
+var titleInput = document.querySelector("#titleInput");
+var bodyInput = document.querySelector("#bodyInput");
+var ideaCardSection = document.querySelector("#ideaCardSection");
+//event listeners
+saveButton.addEventListener('click',)
+//global variables
+var newIdea = new Idea;
+var list = [];
+//functions
